39b854cf7be6e6b288eaa491f6df6ae0c32e609b
Ned Ruggeri <ruggeri@self-loop.com>
2014-04-27 22:02:19 -0700

    user Rails csrf methods
    auth-08

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
-  # DON'T COMMENT ME OUT
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   def current_user
     # fetches the user we've logged in as
@@ -21,18 +19,4 @@ class ApplicationController < ActionController::Base
   def log_out!
     self.session[:session_token] = nil
   end
-
-  # CSRF Stuff
-  def my_csrf_token
-    self.session[:_my_csrf_token] ||= SecureRandom::urlsafe_base64
-  end
-
-  before_filter :validate_csrf_token
-  def validate_csrf_token
-    return if self.request.method == "GET"
-    return if self.session[:_my_csrf_token] == self.params[:_my_csrf_token]
-    raise "CSRF ATTACK DETECTED"
-  end
-
-  helper_method :my_csrf_token
 end
