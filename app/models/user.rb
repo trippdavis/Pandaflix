0a212ed8c3e9301378306207f61b910778a7ab11
Ned Ruggeri <ruggeri@self-loop.com>
2014-11-02 17:00:53 -0800

    eliminate extraneous whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
 
-    # no user with given username    
+    # no user with given username
     return nil if user.nil?
 
     # check user's password
