7b7df59d878ab807031b46897eb850f202e8cc90
Tripp Davis <trippdavis3@gmail.com>
2015-03-15 13:21:27 -0700

    add home contact and about static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 AuthDemo::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/contact"
+  get "static_pages/about"
   root "sessions#new"
 
   resource :session, only: [:create, :destroy, :new]
